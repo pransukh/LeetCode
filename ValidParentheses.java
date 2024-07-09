e8cd19385b78759d36c851927287693734179e63
Pran Sukh <hitman47silent994c4@gmail.com>
2024-07-10 01:52:46 +0530

    added

---
+++ ValidParentheses.java
@@ -0,0 +1,14 @@
+package JAVA.LEETCODE;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        System.out.println(solution("12345"));
+    }
+    static String solution(String input){
+        for (int i = 0; i < input.length() ; i++) {
+            String start = String.valueOf(input.charAt(i));
+
+        }
+        return "";
+    }
+}
