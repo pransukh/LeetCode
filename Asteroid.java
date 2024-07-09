7dd7aade8ba8daa73330d5634c852a795d6cb921
Pran Sukh <hitman47silent994c4@gmail.com>
2024-07-10 01:27:11 +0530

    adding

---
+++ Asteroid.java
@@ -0,0 +1,47 @@
+package JAVA.LEETCODE.CrackInterviewIn75QA;
+
+import edu.princeton.cs.algs4.In;
+
+import java.util.Stack;
+
+public class Asteroid {
+    public static void main(String[] args) {
+//        int asteroids[] = new int[]{10,2,-5};
+//        int asteroids[] = new int[]{-2,-1,1,2};
+//        int asteroids[] = new int[]{8,-8};
+//        int asteroids[] = new int[]{5,10,-5};
+//        int asteroids[] = new int[]{10,2,-5,11,14,100,15,16,-55,17};
+        int asteroids[] = new int[]{10,2,5,11,14,100,15,16,55,17};
+        int[] dom = null;
+
+        Stack<Integer> ans = new Stack();
+        for (int i = 0; i < asteroids.length ; i++) {
+
+            if(!ans.isEmpty() && asteroids[i] < 0){
+                if(Math.abs(ans.peek()) == Math.abs(asteroids[i])){
+                    ans.pop();
+                    continue;
+                }
+               while (!ans.isEmpty() && Math.abs(ans.peek()) < Math.abs(asteroids[i]) ){
+
+                   ans.pop();
+               }
+
+               if(ans.empty() || !ans.isEmpty() && ans.peek() < 0){
+                   ans.add(asteroids[i]);
+               }
+            }else{
+                ans.add(asteroids[i]);
+            }
+        }
+
+        dom = new int[ans.size()];
+        for (int i = 0; i < ans.size() ; i++) {
+            System.out.println(ans.get(i));
+            dom[i] = ans.get(i);
+        }
+
+
+
+    }
+}
