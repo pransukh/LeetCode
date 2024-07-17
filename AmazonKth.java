625aa8b777403c6a12e07dce62a180eccb17324f
Pran Sukh <pransukh.21@gmail.com>
2024-07-17 21:01:08 +0530

    Create AmazonKth.java

---
+++ AmazonKth.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class AmazonKth {
+    public static void main(String[] args) {
+        System.out.println(new AmazonKth().kthFactor(4,4));
+    }
+
+    int kthFactor(int num, int k){
+        int count = 0;
+         for (int i = 1; i <= num ; i++) {
+            if(num % i == 0) {
+                count++;
+            }
+            if(count == k){
+                return  i;
+            }
+        }
+         return -1;
+
+    }
+}
